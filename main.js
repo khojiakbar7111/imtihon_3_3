cabe9aa8656e4f3dcd9c7d46c8a6c7ca9d62f13c
hojiakbar7111 <hojiakbarnematov33@gmail.com>
2023-08-26 11:08:01 +0500

    i can use example

---
+++ main.js
@@ -0,0 +1,17 @@
+// 1-misol
+
+// let arr = ["I","study","JavaScript","right","now"];
+// function delette(arr, del){
+//     return arr.filter(right => right !== del);
+// }
+// let arr2 = delette(arr,"right");
+// console.log(arr2);
+
+
+// 2-misol
+
+
+// let jon = "hello world";
+// console.log(jon.split("").reverse().join(""));
+
+// 3-misol 
